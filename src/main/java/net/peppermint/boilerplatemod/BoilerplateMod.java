5bb0e99d9ae98ed14dc8ca15d00e0225feeced8c
peppermint <peppermintmodeling@gmail.com>
2023-04-22 01:41:43 -0700

    Updated to add sweet_potion, however it does not appear in game

--- src/main/java/net/peppermint/boilerplatemod/BoilerplateMod.java
+++ src/main/java/net/peppermint/boilerplatemod/BoilerplateMod.java
@@ -1,6 +1,7 @@
 package net.peppermint.boilerplatemod;
 
 import net.fabricmc.api.ModInitializer;
+import net.peppermint.boilerplatemod.item.ModItems;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -11,6 +12,6 @@ public class BoilerplateMod implements ModInitializer {
 
 	@Override
 	public void onInitialize() {
-
+		ModItems.registerModItems();
 	}
 }
---
+++ src/main/java/net/peppermint/boilerplatemod/item/ModItems.java
@@ -0,0 +1,39 @@
+package net.peppermint.boilerplatemod.item;
+
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
+import net.minecraft.item.*;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.util.Identifier;
+import net.peppermint.boilerplatemod.BoilerplateMod;
+
+public class ModItems {
+
+//    ItemGroup PEPPERMINT_ITEMS = FabricItemGroup.builder(new Identifier("boilerplatemod", "peppermint_items"))
+//            .displayName(Text.literal("Peppermint's Items"))
+//            .icon(() -> new ItemStack(Items.BEEHIVE))
+//            .build();
+
+    public static final Item SWEET_POTION = registerItem("sweet_potion", new Item(new FabricItemSettings()));
+
+    private static Item registerItem (String name, Item item) {
+        return Registry.register(Registries.ITEM, new Identifier(BoilerplateMod.MOD_ID, name), item);
+    }
+
+    public static void addToItemGroup(ItemGroup group, Item item) {
+        ItemGroupEvents.modifyEntriesEvent(group).register(entries -> entries.add(item));
+    }
+
+    public static void addItemsToItemGroup(){
+        addToItemGroup(ItemGroups.INGREDIENTS, SWEET_POTION);
+    }
+
+    public static void registerModItems(){
+        BoilerplateMod.LOGGER.debug("Registering Mod Items for " + BoilerplateMod.MOD_ID);
+        addItemsToItemGroup();
+    }
+
+
+
+}
