914def72fa6f46a3392e0a0d4384a72879ec3be0
peppermint <peppermintmodeling@gmail.com>
2023-04-22 20:48:25 -0700

    Added Data Generator Class

--- src/main/java/net/peppermint/boilerplatemod/item/ModItems.java
+++ src/main/java/net/peppermint/boilerplatemod/item/ModItems.java
@@ -1,38 +1,37 @@
 package net.peppermint.boilerplatemod.item;
+
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
-import net.minecraft.item.*;
+import net.peppermint.boilerplatemod.BoilerplateMod;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.ItemGroups;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.util.Identifier;
 import net.peppermint.boilerplatemod.BoilerplateMod;
 
 public class ModItems {
+    public static final Item SWEET_POTION = registerItem("sweet_potion",
+            new Item(new FabricItemSettings()));
 
-//    ItemGroup PEPPERMINT_ITEMS = FabricItemGroup.builder(new Identifier("boilerplatemod", "peppermint_items"))
-//            .displayName(Text.literal("Peppermint's Items"))
-//            .icon(() -> new ItemStack(Items.BEEHIVE))
-//            .build();
 
-    private static Item registerItem (String name, Item item) {
+    private static Item registerItem(String name, Item item) {
         return Registry.register(Registries.ITEM, new Identifier(BoilerplateMod.MOD_ID, name), item);
     }
 
-    public static final Item SWEET_POTION = registerItem("sweet_potion", new Item(new FabricItemSettings()));
+    public static void addItemsToItemGroup() {
+        addToItemGroup(ItemGroups.INGREDIENTS, SWEET_POTION);
 
-    public static void addToItemGroup(ItemGroup group, Item item) {
+
+    }
+
+    private static void addToItemGroup(ItemGroup group, Item item) {
         ItemGroupEvents.modifyEntriesEvent(group).register(entries -> entries.add(item));
     }
+    public static void registerModItems() {
+        BoilerplateMod.LOGGER.info("Registering Mod Items for " + BoilerplateMod.MOD_ID);
 
-    public static void addItemsToItemGroup(){
-        addToItemGroup(ItemGroups.INGREDIENTS, SWEET_POTION);
-    }
-
-    public static void registerModItems(){
-        BoilerplateMod.LOGGER.debug("Registering Mod Items for " + BoilerplateMod.MOD_ID);
         addItemsToItemGroup();
     }
-
-
-
 }
